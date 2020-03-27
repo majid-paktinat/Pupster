b0dc2f209313d220b442b0fb1c00f75638e8ffc6
majid <majid.paktinat@gmail.com>
2020-03-27 19:38:43 -0400

    initialize

---
+++ src/components/SearchPage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import BreedSearchForm from './BreedSearchForm';
+import BreedResultList from './BreedResultList';
+
+function SearchPage() {
+  return (
+    <div>
+        <section class="jumbotron text-center">
+            <div class="container">
+                <h1 class="jumbotron-heading">Search</h1>
+                <p class="text-muted lead">Please select the breed of dog</p>
+                <BreedSearchForm />
+            </div>
+        </section>
+
+        <div class="album py-5 bg-light">
+            <BreedResultList />
+        </div>
+    </div>
+  );
+}
+
+export default SearchPage;
